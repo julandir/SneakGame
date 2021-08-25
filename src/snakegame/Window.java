ba24302ad7eb2714befd8114c00a947acd7e830a
= <olstertecn597@gmail.com>
2021-08-25 16:30:48 -0600

    i create the snake class and configure the panel class, also modify the jframe for i can listen the keyboard events when pressed one key

--- src/snakegame/Window.java
+++ src/snakegame/Window.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package snakegame;
+import com.sun.glass.events.KeyEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -15,6 +16,33 @@ public class Window  {
     //constructor method
     public void CreateWindow(){
         JFrame marco = new JFrame("texto");
+        
+        
+        
+        marco.addKeyListener(new java.awt.event.KeyAdapter(){
+            public void keyReleased(java.awt.event.KeyEvent evt){
+                if(evt.getKeyCode() == KeyEvent.VK_UP){
+                    System.out.println("up key");
+                }
+                else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
+                    System.out.println("down key");
+                }
+                else if(evt.getKeyCode() == KeyEvent.VK_LEFT){
+                    System.out.println("left key");
+                }
+                else if(evt.getKeyCode() == KeyEvent.VK_RIGHT){
+                    System.out.println("right key");
+                }
+                else
+                {
+                    System.out.println("other key");
+                }
+            }
+        });
+        
+        
+        
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         marco.add(new panel());
         marco.setSize(800,600);
         marco.setVisible(true);
